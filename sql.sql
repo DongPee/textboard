55ded26e3a9cc0cebddb3057f0fcaa8a5189c548
DongPee <dongh719@naver.com>
2023-06-15 16:18:54 +0900

    sql 수정

--- sql.sql
+++ sql.sql
@@ -4,14 +4,14 @@ CREATE TABLE `게시글` (
 	`내용` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`작성일자` VARCHAR(50) NOT NULL DEFAULT curdate() COLLATE 'utf8mb4_general_ci',
 	`고유번호` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`비밀번호` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
 	PRIMARY KEY (`고유번호`) USING BTREE
 )
 COMMENT='작성자, 제목, 내용, 작성일자, 고유 번호'
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=3
+AUTO_INCREMENT=30
 ;
-
 
 CREATE TABLE `댓글` (
 	`작성자` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
@@ -19,9 +19,12 @@ CREATE TABLE `댓글` (
 	`고유번호` INT(11) NOT NULL AUTO_INCREMENT,
 	`내용` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`게시글 고유번호` INT(11) UNSIGNED NOT NULL,
-	PRIMARY KEY (`고유번호`) USING BTREE
+	`비밀번호` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`고유번호`) USING BTREE,
+	INDEX `FK_댓글_게시글` (`게시글 고유번호`) USING BTREE,
+	CONSTRAINT `FK_댓글_게시글` FOREIGN KEY (`게시글 고유번호`) REFERENCES `게시글` (`고유번호`) ON UPDATE CASCADE ON DELETE CASCADE
 )
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=2
+AUTO_INCREMENT=20
 ;
